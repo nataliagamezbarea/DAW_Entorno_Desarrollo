fd6f6ad4d2bd33a2001cf76064617859d48dc33d
nataliagamezbarea <nataliagbarea@gmail.com>
2025-01-09 12:35:17 +0100

    Añadir tarea

--- 2-Flask_CRUD_Tareas/app copy.py
+++ 2-Flask_CRUD_Tareas/app copy.py
@@ -3,34 +3,32 @@ import json
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def index():
     title = "Bienvenido a Flask con Jinja2"
     description = "Esta página demuestra las características principales de Jinja2."
     return render_template("index.html", title=title, description=description)
 
-
 @app.route('/form', methods=['GET', 'POST'])
 def form():
-    with open ('data/tasks.json', encoding='utf-8') as  file :
-        tasks=json.load(file)
+    # Leer el archivo JSON
+    with open('data/tasks.json', encoding='utf-8') as file:
+        tasks = json.load(file)
 
     if request.method == 'POST':
         task_name = request.form['task_name']
         task_description = request.form['task_description']
-
-        # Mostrar JSON donde contienen las tareas es mejor tenerlo afuera de template (porque en templates irán los archivos HTML)
-        new_task = {"name": task_name, "description": task_description}
+        
+        # Crear una nueva tarea
         new_task = {"name": task_name, "description": task_description}
         tasks.append(new_task)  # Agregar la nueva tarea
 
-    # write the file tasks.json
-    with open('data/tasks.json', 'w', encoding='utf-8') as file:
-        json.dump(tasks, file, ensure_ascii=False, indent=4)
+        # Sobrescribir el archivo tasks.json
+        with open('data/tasks.json', 'w', encoding='utf-8') as file:
+            json.dump(tasks, file, ensure_ascii=False, indent=4)
 
+    # Mostrar las tareas en el formulario
     return render_template("form.html", tasks=tasks)
-
 
 if __name__ == "__main__":
     app.run(debug=True)
